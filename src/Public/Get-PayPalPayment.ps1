5449a244b9cb32721efee2c3c8ebd49096e6deab
Anthony Howell <anthony@howell-it.com>
2018-11-15 14:07:37 -0800

    Can now retrieve an order from PayPal

---
+++ src/Public/Get-PayPalPayment.ps1
@@ -0,0 +1,17 @@
+# https://developer.paypal.com/docs/api/orders/v1/#orders_get
+Function Get-PayPalOrder {
+    Param(
+        [string]$OrderID,
+        [string]$AccessToken = $PayPalAuthConfig.AccessToken
+    )
+    $baseUri = 'https://api.paypal.com/v1/checkout/orders'
+
+    $uri = "$baseUri/$orderID"
+
+    $headers = @{
+        'Authorization' = "Bearer $AccessToken"
+        'Content-Type' = 'application/json'
+    }
+
+    Invoke-RestMethod -Uri $uri -Method Get -Headers $headers
+}
